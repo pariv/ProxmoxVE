ad27702f1feffa424c18358896fc9ab1867499ac
Ivan Parfentyev <pariv@mail.ru>
2025-05-17 13:57:49 +0300

    change place

--- install/immich-install.sh
+++ install/immich-install.sh
@@ -185,6 +185,18 @@ cd -
 SOURCE_DIR="/root/image-source"
 $STD mkdir -p "$SOURCE_DIR"
 
+# --- libheif ---
+LIBHEIF_REVISION=$(jq -cr '.revision' $BASE_IMG_REPO_DIR/server/sources/libheif.json)
+$STD git clone https://github.com/strukturag/libheif.git $SOURCE_DIR/libheif
+cd $SOURCE_DIR/libheif
+$STD git reset --hard "$LIBHEIF_REVISION"
+$STD rm -rf build && $STD mkdir build && cd build
+$STD cmake --preset=release-noplugins -DWITH_DAV1D=ON -DENABLE_PARALLEL_TILE_DECODING=ON -DWITH_LIBSHARPYUV=ON -DWITH_LIBDE265=ON -DWITH_AOM_DECODER=OFF -DWITH_AOM_ENCODER=OFF -DWITH_X265=OFF -DWITH_EXAMPLES=OFF ..
+# $STD cmake --preset=release-noplugins -DWITH_DAV1D=ON -DENABLE_PARALLEL_TILE_DECODING=ON -DWITH_LIBSHARPYUV=ON -DWITH_LIBDE265=ON -DWITH_AOM_DECODER=OFF -DWITH_AOM_ENCODER=OFF -DWITH_X265=OFF -DWITH_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-DLIBJPEG_TURBO_VERSION_NUMBER=$LIBJPEG_TURBO_VERSION_NUMBER" ..
+$STD make install -j "$(nproc)"
+$STD ldconfig /usr/local/lib
+
+
 # --- libjxl ---
 LIBJXL_REVISION=$(jq -cr '.revision' $BASE_IMG_REPO_DIR/server/sources/libjxl.json)
 $STD git clone https://github.com/libjxl/libjxl.git $SOURCE_DIR/libjxl
@@ -197,17 +209,6 @@ $STD rm -rf build && $STD mkdir build && cd build
 $STD cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_DOXYGEN=OFF -DJPEGXL_ENABLE_MANPAGES=OFF -DJPEGXL_ENABLE_PLUGIN_GIMP210=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=ON -DJPEGXL_FORCE_SYSTEM_HWY=OFF -DJPEGXL_ENABLE_JPEGLI=ON -DJPEGXL_ENABLE_JPEGLI_LIBJPEG=ON -DJPEGXL_INSTALL_JPEGLI_LIBJPEG=ON -DJPEGXL_ENABLE_PLUGINS=ON ..
 $STD cmake --build . -- -j"$(nproc)"
 $STD cmake --install .
-$STD ldconfig /usr/local/lib
-
-# --- libheif ---
-LIBHEIF_REVISION=$(jq -cr '.revision' $BASE_IMG_REPO_DIR/server/sources/libheif.json)
-$STD git clone https://github.com/strukturag/libheif.git $SOURCE_DIR/libheif
-cd $SOURCE_DIR/libheif
-$STD git reset --hard "$LIBHEIF_REVISION"
-$STD rm -rf build && $STD mkdir build && cd build
-$STD cmake --preset=release-noplugins -DWITH_DAV1D=ON -DENABLE_PARALLEL_TILE_DECODING=ON -DWITH_LIBSHARPYUV=ON -DWITH_LIBDE265=ON -DWITH_AOM_DECODER=OFF -DWITH_AOM_ENCODER=OFF -DWITH_X265=OFF -DWITH_EXAMPLES=OFF ..
-# $STD cmake --preset=release-noplugins -DWITH_DAV1D=ON -DENABLE_PARALLEL_TILE_DECODING=ON -DWITH_LIBSHARPYUV=ON -DWITH_LIBDE265=ON -DWITH_AOM_DECODER=OFF -DWITH_AOM_ENCODER=OFF -DWITH_X265=OFF -DWITH_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-DLIBJPEG_TURBO_VERSION_NUMBER=$LIBJPEG_TURBO_VERSION_NUMBER" ..
-$STD make install -j "$(nproc)"
 $STD ldconfig /usr/local/lib
 
 # --- libraw ---
